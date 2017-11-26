c925e08f800553a946947000a704e31651b5019d
aykiel <aykiel@gmail.com>
2017-11-26 16:07:42 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace complex_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                MessageBox.Show("radioButton1");
+            }
+            else if (radioButton2.Checked == true)
+            {
+                MessageBox.Show("radioButton2");
+            }
+            
+        }
+    }
+}
